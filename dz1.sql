24fbcc1eafba94609eae910d9eb4c76c903df11f
Vadim Yevstiunin <114993175+Vadim29091981@users.noreply.github.com>
2023-05-21 23:34:50 +0200

    Add files via upload

---
+++ dz1.sql
@@ -0,0 +1,5 @@
+select * from employees where department_id = 90;
+select * from employees where salary > 5000;
+select * from employees where last_name like "L%";
+select * from departments where location_id = 1700;
+select * from locations where country_id = "US";
